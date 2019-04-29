eccd02183e084ba92f24dc6c07c9920580f07f3e
pouya-moh <other.pouya@gmail.com>
2019-04-29 17:51:11 +0200

    basic version working

--- auto-glossaries-mode.el
+++ auto-glossaries-mode.el
@@ -1,55 +1,52 @@
 (provide 'auto-gls-mode)
 
-(setq agls-acn-file "root.acn")
-(set-buffer (find-file-noselect agls-acn-file))
-
+(setq agls-acn-file "acronyms.tex")
+(setq agls-list '())
 
 
 (defun agls-parse-acn-file()
-  (goto-char 1)
-  (setq agls-list '())
-  (while moreLines
-    (search-forward "glossentry{")
-    (set-mark (point))
-    (search-forward "}")
-    (backward-char 1)
-    (add-to-list 'agls-list (buffer-substring-no-properties (region-beginning) (region-end)))
-    (setq moreLines (= 0 (forward-line 1)))
-    )
-  )
+  (setq acro-list '())
+  ;(set-buffer (find-file-noselect agls-acn-file))
+  (with-temp-buffer
+    (insert-file-contents agls-acn-file)
+    (goto-char 1)
+    (setq eol-flag nil)
+    (while (not eol-flag)
+      (search-forward "newacronym{")
+      (set-mark (point))
+      (search-forward "}")
+      (backward-char 1)
+      (add-to-list 'acro-list (buffer-substring-no-properties (region-beginning) (region-end)))
+      (forward-line 1)
+      (setq eol-flag (eq (point) (point-max)))))
+  acro-list)
 
 (defun agls-acronym ()
   (backward-sexp)
   (insert "\\gls{")
   (forward-sexp)
-  (insert "}")
-  )
+  (insert "}"))
 
 (defun agls-check-membership()
   (save-excursion
   (backward-char 1)
   (when (member (thing-at-point 'sexp) agls-list)
-    (agls-acronym)
-    )
-  )
-  )
+    (agls-acronym))))
 
 (defun agls-check-word ()
   "Check if the word is in the acronym list"
+  (message "ffff")
   (if (= last-command-event 32)
-      (agls-check-membership))
-  )   
+      (agls-check-membership)))   
 
 (define-minor-mode auto-gls-mode
   "Documentation goes here"
-  :lighter " a-gls"
-  (setq agls-list '("com" "foo" "svg"))
+  :lighter " AGLS"
+;  (setq agls-list '("foo" "bar"))
+  (setq agls-list (agls-parse-acn-file))
   (if auto-gls-mode
       (add-hook 'post-self-insert-hook
 		'agls-check-word nil t)
     (remove-hook 'post-self-insert-hook
-		 'agls-check-word T)
-      )
-  )
-
+		 'agls-check-word t)))
 
