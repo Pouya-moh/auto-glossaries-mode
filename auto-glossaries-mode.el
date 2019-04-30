d73855e5a4036bcf0f7bc59c66df88ec5a3239a3
pouya-moh <other.pouya@gmail.com>
2019-04-30 09:58:21 +0200

    add some documentation and refactored some names

--- auto-glossaries-mode.el
+++ auto-glossaries-mode.el
@@ -1,14 +1,44 @@
+;;; auto-gls-mode.el
+;;
+;; This little minor mode parses an acronym file and creates a list
+;; out of it first. The aim is to facilitate the latex workflow of
+;; typing \gls{foo} dicated by glossaries package. In this case, foo
+;; is extraced from acronym file and when user types foo, the minor
+;; mode automatically replaces it with \gls{foo}
+;;
+;;
+;;
+;;        Author: Pouya-moh
+;;           URL: https://github.com/Pouya-moh/auto-glossaries-mode
+;;       Version: 0.1
+;; Dependencies : none
+;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+
 (provide 'auto-gls-mode)
 
-(setq agls-acn-file "acronyms.tex")
-(setq agls-list '())
+(setq agls-acronym-file "acronyms.tex")
+(setq agls-acronym-list '())
 
 
-(defun agls-parse-acn-file()
+(defun agls-parse-acronym-file()
+  "Parses and the file, extracts acronyms, and store them in a list"
   (setq acro-list '())
-  ;(set-buffer (find-file-noselect agls-acn-file))
   (with-temp-buffer
-    (insert-file-contents agls-acn-file)
+    (insert-file-contents agls-acronym-file)
     (goto-char 1)
     (setq eol-flag nil)
     (while (not eol-flag)
@@ -21,32 +51,32 @@
       (setq eol-flag (eq (point) (point-max)))))
   acro-list)
 
-(defun agls-acronym ()
+(defun agls-acronymize ()
+  "This is the string that will surround the typed acronym"
   (backward-sexp)
   (insert "\\gls{")
   (forward-sexp)
   (insert "}"))
 
 (defun agls-check-membership()
+  "check weather the last sexp is in the acronym list"
   (save-excursion
   (backward-char 1)
-  (when (member (thing-at-point 'sexp) agls-list)
-    (agls-acronym))))
+  (when (member (thing-at-point 'sexp) agls-acronym-list)
+    (agls-acronymize))))
 
-(defun agls-check-word ()
-  "Check if the word is in the acronym list"
-  (message "ffff")
+(defun agls-check-if-word ()
+  "If the last key stroke was whitespace, then check for sexp membership in the list"
   (if (= last-command-event 32)
       (agls-check-membership)))   
 
 (define-minor-mode auto-gls-mode
-  "Documentation goes here"
+  "A simple minor mode to automatically surrond acronyms in gls macro in latex mode"
   :lighter " AGLS"
-;  (setq agls-list '("foo" "bar"))
-  (setq agls-list (agls-parse-acn-file))
+  (setq agls-acronym-list (agls-parse-acronym-file))
   (if auto-gls-mode
       (add-hook 'post-self-insert-hook
-		'agls-check-word nil t)
+		'agls-check-if-word nil t)
     (remove-hook 'post-self-insert-hook
-		 'agls-check-word t)))
+		 'agls-check-if-word t)))
 
